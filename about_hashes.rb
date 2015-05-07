582fdf4a81541f7eab5f74f47bfe5550736c5a79
Tarek Khalaf <pcgeek191@gmail.com>
2015-05-07 23:51:10 +0300

    Some merging for hashes

--- about_hashes.rb
+++ about_hashes.rb
@@ -1,5 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+h1 = { "a" => 111, "b" => 222 }
+h2 = { "b" => 333, "c" => 444 }
+h1.merge(h2)
+h2.merge(h1)
+h1.merge(h2) {|key,old,new| new  }
+h1.merge(h2) {|key,old,new| old }
+
+h1.merge(h2) {|key,old,new| old * 2 }
+
+h1.merge(h2) {|key,old,new| old * 5 }
+
+h1.merge(h2) do |key, old, new|
+  if old < new
+    old
+  else
+    new
+  end
+end
+h1.merge(h2) {|k,o,n| o < n ? o : n }
+h1
+h2
+h1.merge!(h2)# Makes h1 permenant change
+h1
+
+
 class AboutHashes < Neo::Koan
   def test_creating_hashes
     empty_hash = Hash.new
