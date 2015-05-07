966c1e51e5a9136b6a811a56c19a5d9fdce7fe75
Tarek Khalaf <pcgeek191@gmail.com>
2015-05-08 00:12:33 +0300

    Some array methods

--- about_arrays.rb
+++ about_arrays.rb
@@ -1,5 +1,54 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+
+array = [1,2,3,4,5]
+array2 = [1, "2", 3.0, ["a", "b"], "dog"]
+array.inspect
+array
+puts array
+
+puts array.inspect
+
+puts array2.inspect
+
+puts array2
+
+array2.to_s
+array2.join
+array2.join(", ")
+x = "1,2,3,4,5"
+y = x.split(',')
+y
+y.reverse
+array
+array << 0
+array.sort
+array2.sort
+
+array << 3
+array.uniq
+array
+array.uniq!
+array
+array.delete_at(2)
+array
+array.delete(4)
+array
+array << 3
+array.push(4)
+array.pop
+array
+array.shift
+array
+array.unshift(1)
+array
+array + [9, 10, 11, 12]
+array
+new_array = array + [9,10]
+new_array = array - [9,10]
+new_array = array - [2]
+
+
 class AboutArrays < Neo::Koan
   def test_creating_arrays
     empty_array = Array.new
