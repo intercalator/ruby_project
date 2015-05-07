5e89cb0f9fa40af103e709fd6afb57ab1fad8808
Tarek Khalaf <pcgeek191@gmail.com>
2015-05-07 23:54:37 +0300

    Custom methods

--- about_methods.rb
+++ about_methods.rb
@@ -4,6 +4,36 @@ def my_global_method(a,b)
   a + b
 end
 
+def welcome
+	puts "Hello World!"
+end
+
+def add
+	puts 1 + 1
+end
+
+# Underscore between words, like variable names
+def longest_word
+  words = ['apple', 'pear', 'banana', 'plum']
+  longest_word = words.inject do |memo,word|
+    memo.length > word.length ? memo : word
+  end
+  puts longest_word
+end
+
+# Method names can have question marks in them
+# Useful for tests and booleans
+def over_five?
+	value = 3
+	puts value > 5 ? 'Over 5' : 'Not over 5'
+end
+
+# must define methods before calling them!
+welcome
+add
+longest_word
+over_five?
+
 class AboutMethods < Neo::Koan
 
   def test_calling_global_methods
